0a5da2b03c8fdc712f9b7be866eeb51f3b9dbf04
yangoff <yangovip@gmail.com>
2018-05-22 18:31:38 -0300

    Passo 3

---
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kai
+ * Date: 22/05/2018
+ * Time: 18:03
+ */
+
+namespace Hcode;
+use  Rain\Tpl;
+
+class Page{
+    private $tpl;
+    private  $options = [];
+    private  $defaults = [
+      "data"=>[]
+    ];
+    public function __construct($opts = array()){
+        $this->options = array_merge($this->defaults,$opts);
+
+
+        $config = array(
+          "tpl_dir" => $_SERVER["DOCUMENT_ROOT"]."/views/",
+          "cache_dir" => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+          "debug" => true
+        );
+            Tpl::configure($config);
+            $this->tpl = new Tpl();
+
+        $this->setData($this->options["data"]);
+
+            $this->tpl->draw("header");
+
+    }
+
+    private function setData($data = array()){
+        foreach ($data as $key => $value){
+
+            $this->tpl->assign($key, $value);
+
+        }
+    }
+
+    public function setTpl($name,$data = array(),$returnHTML = false){
+
+        $this->setData($data);
+        return $this->tpl->draw($name,$returnHTML);
+    }
+
+    public function __destruct(){
+        $this->tpl->draw("footer");
+    }
+
+}
---
+++ views-cache/header.ddc6f38b37dc4abe1df6911be47e773f.rtpl.php
@@ -0,0 +1,8 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
