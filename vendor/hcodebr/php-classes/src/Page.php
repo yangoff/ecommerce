3c82460f72562b4a29c5aeacae13f943f84084b7
yangoff <yangovip@gmail.com>
2018-05-23 17:07:26 -0300

    passo 5

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -13,6 +13,8 @@ class Page{
     private $tpl;
     private  $options = [];
     private  $defaults = [
+        "header"=>true,
+        "footer"=>true,
       "data"=>[]
     ];
     public function __construct($opts = array(),$tpl_dir = "/views/"){
@@ -29,7 +31,8 @@ class Page{
 
         $this->setData($this->options["data"]);
 
-            $this->tpl->draw("header");
+            if ($this->options["header"]===true)$this->tpl->draw("header");
+
 
     }
 
@@ -48,7 +51,7 @@ class Page{
     }
 
     public function __destruct(){
-        $this->tpl->draw("footer");
+        if ($this->options["footer"]===true) $this->tpl->draw("footer");
     }
 
 }
